694414c9fd4e2532554f63b8b126ba3e64764042
maorar <m_ormachea@hotamail.com>
2022-01-19 20:18:48 -0400

    initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  BASE_URL: "https://fstack-ecommerce.herokuapp.com"
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  production: false,
+  BASE_URL: "https://fstack-ecommerce.herokuapp.com"
+};
