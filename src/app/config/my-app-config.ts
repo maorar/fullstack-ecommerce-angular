8e4c34b41605cbb0461ba92262fa03b05477e48e
maorar <m_ormachea@hotamail.com>
2022-01-19 20:55:02 -0400

    my-app-config changed to heroku front

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId:'0oa3m00xblnz0hpEj5d7',
     issuer:'https://dev-28354030.okta.com/oauth2/default',
-    redirectUri:'http://localhost:4200/login/callback',
+    redirectUri:'https://fstack-ecommerce-front.herokuapp.com/login/callback',
     scopes:['openid','profile','email']
   }
   }
